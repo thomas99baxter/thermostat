1124e174d18d67d54dff96af0d87fc9301cad93f
Tom Baxter <thomas99baxter@gmail.com>
2021-10-25 16:00:07 +0100

    add down method and temp limits

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,14 +1,41 @@
 const Thermostat = require("../lib/thermostat.js");
 
 describe("Thermostat", () => {
+  let test_thermostat;
+  beforeEach(() => {
+    test_thermostat = new Thermostat();
+  })
+
   it("starts at 20 degrees", () => {
-    let test_thermostat = new Thermostat();
     expect(test_thermostat.getCurrentTemperature()).toEqual(20);
   });
 
   it("You can increase the temperature with an up function", () => {
-    let test_thermostat = new Thermostat();
     test_thermostat.up();
     expect(test_thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it("You can increase the temperature with an up function", () => {
+    test_thermostat.down();
+    expect(test_thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it("The temperature will not decrease below 10 degrees", () => {
+
+    for(let i = 0; i < 10; i++){
+      test_thermostat.down();
+    }
+    expect(test_thermostat.getCurrentTemperature()).toEqual(10);
+    test_thermostat.down();
+    expect(test_thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it("Temperature should not go over 25 on power saving mode", () => {
+    for(let i = 0; i < 5; i++){
+      test_thermostat.up();
+    }
+    expect(test_thermostat.getCurrentTemperature()).toEqual(25);
+    test_thermostat.up()
+    expect(test_thermostat.getCurrentTemperature()).toEqual(25);
   });
 });
