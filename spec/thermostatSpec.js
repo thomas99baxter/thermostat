cff9bcc24150dde33a701cc534434d0a7abb424d
Steve Jones <steve.jones763@gmail.com>
2021-10-25 15:42:39 +0100

    Temp can now be increased with the up function.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,4 +5,10 @@ describe("Thermostat", () => {
     let test_thermostat = new Thermostat();
     expect(test_thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it("You can increase the temperature with an up function", () => {
+    let test_thermostat = new Thermostat();
+    test_thermostat.up();
+    expect(test_thermostat.getCurrentTemperature()).toEqual(21);
+  });
 });
